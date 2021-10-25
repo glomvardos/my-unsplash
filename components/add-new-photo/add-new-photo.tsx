bfabe95951fedb74bdb61ae2bba01ade2629b433
glomvardos <glomvardos@gmail.com>
2021-10-25 22:16:02 +0300

    Added form submit handler

--- components/add-new-photo/add-new-photo.tsx
+++ components/add-new-photo/add-new-photo.tsx
@@ -7,11 +7,14 @@ import Modal from '../modal/modal'
 
 export default function AddNewPhoto() {
   const { showModalHandler } = useContext(store)
+  const onSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault()
+  }
 
   return (
     <Modal onClickHandler={showModalHandler}>
       <FormTitle text='Add a new photo' />
-      <form>
+      <form onSubmit={onSubmitHandler}>
         <Input type='text' label='Label' placeholder='Add a label' />
         <Input type='text' label='Photo URL' placeholder='Enter your photo URL' />
         <div className='text-right'>
