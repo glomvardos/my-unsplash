2d362772d0eed5bcb230a853da7f944f92233fc5
glomvardos <glomvardos@gmail.com>
2021-11-02 23:20:20 +0200

    Added search function and state

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -1,6 +1,11 @@
+import { useState } from 'react'
 import { BiSearchAlt2 } from 'react-icons/bi'
 
 export default function Search({ images }: Props) {
+  const [enteredInput, setEnteredInput] = useState<string>('')
+
+  const onSearchHandler = () => {}
+
   return (
     <div className='flex items-center w-[160px] xs:w-[200px] sm:w-[300px] ml-5 sm:ml-10 py-1 xs:py-[14px] px-2 sm:px-5 border-[1px] rounded-xl border-[#BDBDBD]'>
       <BiSearchAlt2 className='text-[#BDBDBD] text-lg xs:text-2xl font-medium' />
@@ -8,6 +13,8 @@ export default function Search({ images }: Props) {
         className='w-full font-normal text-base outline-none ml-2 sm:ml-5 text-[#4F4F4F]'
         type='text'
         placeholder='Search by name'
+        value={enteredInput}
+        onChange={e => setEnteredInput(e.target.value)}
       />
     </div>
   )
